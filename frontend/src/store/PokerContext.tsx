0b331fdb11433ff6f644692a57642ab6d618d86a
Mateusz Szymański <Mizerykordia12@interia.pl>
2025-12-16 18:27:36 +0100

    feat: add initial PokerContext without blinds and breaks

---
+++ frontend/src/store/PokerContext.tsx
@@ -0,0 +1,106 @@
+import { createContext, useState, type ReactNode } from 'react';
+
+type GameSettings = {
+	// basic input data
+	startingStack: number;
+	buyInValue: number;
+	buyIns: number;
+	rebuys: number;
+	playersIn: number;
+
+	// calculated data
+	averageStack: number;
+	totalChips: number;
+	totalMoney: number;
+};
+
+type PokerContextSettings = {
+	settings: GameSettings;
+	setStartingStack: (count: number) => void;
+	setBuyInValue: (value: number) => void;
+	setBuyInsCount: (count: number) => void;
+	setRebuysCount: (count: number) => void;
+	setPlayersInCount: (count: number) => void;
+};
+
+const PokerContext = createContext<PokerContextSettings | undefined>(undefined);
+
+export const PokerProvider = ({ children }: { children: ReactNode }) => {
+	const [settings, setSettings] = useState<GameSettings>({
+		startingStack: 2500,
+		buyInValue: 100,
+		buyIns: 7,
+		rebuys: 0,
+		playersIn: 7,
+		averageStack: 2,
+		totalChips: 2,
+		totalMoney: 2,
+	});
+
+	const setStartingStack = (stack: number) => {
+		setSettings((prev) => ({ ...prev, startingStack: stack }));
+	};
+
+	const setBuyInValue = (value: number) => {
+		setSettings((prev) => ({ ...prev, buyInValue: value }));
+	};
+
+	const setBuyInsCount = (count: number) => {
+		setSettings((prev) => {
+			const newTotalChips = (count + prev.rebuys) * prev.startingStack;
+			const newAverageStack = Math.floor(newTotalChips / prev.playersIn);
+			const newTotalMoney = (count + prev.rebuys) * prev.buyInValue;
+
+			return {
+				...prev,
+				buyIns: count,
+				totalChips: newTotalChips,
+				averageStack: newAverageStack,
+				totalMoney: newTotalMoney,
+			};
+		});
+	};
+
+	const setRebuysCount = (count: number) => {
+		setSettings((prev) => {
+			const newTotalChips = (prev.buyIns + count) * prev.startingStack;
+			const newAverageStack = Math.floor(newTotalChips / prev.playersIn);
+			const newTotalMoney = (prev.buyIns + count) * prev.buyInValue;
+
+			return {
+				...prev,
+				rebuys: count,
+				totalChips: newTotalChips,
+				averageStack: newAverageStack,
+				totalMoney: newTotalMoney,
+			};
+		});
+	};
+
+	const setPlayersInCount = (count: number) => {
+		setSettings((prev) => {
+			const newAverageStack = Math.floor(prev.totalChips / count);
+
+			return {
+				...prev,
+				playersIn: count,
+				averageStack: newAverageStack,
+			};
+		});
+	};
+
+	return (
+		<PokerContext.Provider
+			value={{
+				settings,
+				setStartingStack,
+				setBuyInValue,
+				setBuyInsCount,
+				setRebuysCount,
+				setPlayersInCount,
+			}}
+		>
+			{children}
+		</PokerContext.Provider>
+	);
+};
