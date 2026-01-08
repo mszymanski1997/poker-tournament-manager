2d4f98386ea6ac9d2d33b71527ba8f35eb6c6094
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-01-08 11:03:48 +0100

    feat(tournament): add restart confirmation modal

--- frontend/src/pages/Timer/CenterPanel/CentralPanel.tsx
+++ frontend/src/pages/Timer/CenterPanel/CentralPanel.tsx
@@ -8,6 +8,8 @@ import {
 	MdPause,
 } from 'react-icons/md';
 import { useTimerSettings } from '../../../store/TimerSettings/useTimerSettings';
+import Modal from '../../../components/shared/Modal/Modal';
+import { useState } from 'react';
 
 type CentralPanelProps = {
 	onOpenSettings: () => void;
@@ -29,52 +31,76 @@ const CentralPanel = ({ onOpenSettings }: CentralPanelProps) => {
 
 	const isBlind = currentLevel.type === 'blind';
 
-	return (
-		<div className={styles.container}>
-			<TimerCounter />
-			{!isTournamentFinished && (
-				<div className={styles.blindsInfo}>
-					<p>Current Blinds:</p>
-					<p className={styles.blinds}>
-						{isBlind
-							? currentLevel.bigBlind + ' / ' + currentLevel.smallBlind
-							: 'BREAK'}
-					</p>
-					<p>
-						Ante: <span>{isBlind ? currentLevel.ante : '-'}</span>
-					</p>
-				</div>
-			)}
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
-			<div className={styles.timerButtons}>
-				<Button onClick={previousLevel} disabled={isFirstLevel}>
-					<MdSkipPrevious />
-				</Button>
-				{isRunning ? (
-					<Button onClick={stopTimer}>
-						<MdPause />
-					</Button>
-				) : (
-					<Button onClick={startTimer} disabled={isTournamentFinished}>
-						<MdPlayArrow />
-					</Button>
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	const restart = () => {
+		restartTournament();
+		setIsModalOpen(false);
+	};
+
+	return (
+		<>
+			<Modal isOpen={isModalOpen} onClose={closeModal}>
+				<div className={styles.restartContainer}>
+					<h2>Are you sure that you want to restart tournament?</h2>
+					<div className={styles.restartButtons}>
+						<Button onClick={closeModal}>Cancel</Button>
+						<Button onClick={restart} className={styles.buttonDanger}>
+							Restart
+						</Button>
+					</div>
+				</div>
+			</Modal>
+			<div className={styles.container}>
+				<TimerCounter />
+				{!isTournamentFinished && (
+					<div className={styles.blindsInfo}>
+						<p>Current Blinds:</p>
+						<p className={styles.blinds}>
+							{isBlind
+								? currentLevel.bigBlind + ' / ' + currentLevel.smallBlind
+								: 'BREAK'}
+						</p>
+						<p>
+							Ante: <span>{isBlind ? currentLevel.ante : '-'}</span>
+						</p>
+					</div>
 				)}
 
-				<Button onClick={nextLevel} disabled={isLastLevel}>
-					<MdSkipNext />
-				</Button>
-			</div>
+				<div className={styles.timerButtons}>
+					<Button onClick={previousLevel} disabled={isFirstLevel}>
+						<MdSkipPrevious />
+					</Button>
+					{isRunning ? (
+						<Button onClick={stopTimer}>
+							<MdPause />
+						</Button>
+					) : (
+						<Button onClick={startTimer} disabled={isTournamentFinished}>
+							<MdPlayArrow />
+						</Button>
+					)}
 
-			<div className={styles.settingsButtons}>
-				<Button size='big'>Load settings</Button>
-				<Button size='big' onClick={restartTournament}>
-					Restart
-				</Button>
-				<Button size='big' onClick={onOpenSettings}>
-					{isTournamentFinished ? 'Add new blinds' : 'Custom settings'}
-				</Button>
+					<Button onClick={nextLevel} disabled={isLastLevel}>
+						<MdSkipNext />
+					</Button>
+				</div>
+
+				<div className={styles.settingsButtons}>
+					<Button size='big'>Load settings</Button>
+					<Button size='big' onClick={() => setIsModalOpen(true)}>
+						Restart
+					</Button>
+					<Button size='big' onClick={onOpenSettings}>
+						{isTournamentFinished ? 'Add new blinds' : 'Custom settings'}
+					</Button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
