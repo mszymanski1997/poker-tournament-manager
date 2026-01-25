528b584f3e00a2f297f25ace3a1578b2135a8f93
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-01-25 18:17:37 +0100

    style(form): improve layout and spacing for desktop

--- frontend/src/store/TimerSettings/defaultLevels.ts
+++ frontend/src/store/TimerSettings/defaultLevels.ts
@@ -3,7 +3,7 @@ import type { Level } from './types';
 export const DEFAULT_LEVELS: Level[] = [
 	{
 		type: 'blind',
-		duration: 0.05,
+		duration: 1,
 		bigBlind: 100,
 		smallBlind: 50,
 		ante: 100,
@@ -11,7 +11,7 @@ export const DEFAULT_LEVELS: Level[] = [
 	},
 	{
 		type: 'blind',
-		duration: 0.05,
+		duration: 1,
 		bigBlind: 200,
 		smallBlind: 100,
 		ante: 200,
@@ -19,7 +19,7 @@ export const DEFAULT_LEVELS: Level[] = [
 	},
 	{
 		type: 'blind',
-		duration: 0.05,
+		duration: 1,
 		bigBlind: 300,
 		smallBlind: 150,
 		ante: 300,
@@ -27,7 +27,7 @@ export const DEFAULT_LEVELS: Level[] = [
 	},
 	{
 		type: 'blind',
-		duration: 0.05,
+		duration: 1,
 		bigBlind: 400,
 		smallBlind: 200,
 		ante: 400,
