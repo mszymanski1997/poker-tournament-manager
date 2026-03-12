1530e7c233eebfb257b787cd52b41160dc5ffb24
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-03-12 21:19:48 +0100

    refactor(form): change Input styles to global SCSS and add select styling

---
+++ frontend/src/components/shared/FormElements/Input.tsx
@@ -0,0 +1,35 @@
+import type { InputHTMLAttributes } from 'react';
+
+type InputProps = {
+	label: string;
+	minValue?: number;
+	warning?: boolean;
+	error?: boolean;
+	warningText?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const Input = ({
+	label,
+	type = 'number',
+	minValue = 0,
+	error = false,
+	warningText,
+	...props
+}: InputProps) => {
+	return (
+		<div className='wrapper'>
+			<label className='label'>
+				<span>{label}</span>
+
+				<input
+					type={type}
+					min={minValue}
+					className={`input ${error ? 'warningBackground' : 'normalInput'}`}
+					{...props}
+				/>
+			</label>
+			<p className='warningMessage'>{error ? warningText : '\u00A0'}</p>
+		</div>
+	);
+};
+export default Input;
