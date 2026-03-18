5d4babd70238cfad3cb1a925e90137d65456eb44
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-03-18 18:12:08 +0100

    feat(advanced-settings): add toggle for advanced settings with consistent layout
    - show input and remove button after checkbox click
    - restore checkbox when input is removed
    - style layout so inputs and questions have equal width
    - improve responsiveness across breakpoints"

--- frontend/src/components/shared/Form/TopSettingsSection/AdvancedSettings.tsx
+++ frontend/src/components/shared/Form/TopSettingsSection/AdvancedSettings.tsx
@@ -1,16 +1,61 @@
+import { useState } from 'react';
 import styles from './AdvancedSettings.module.scss';
+import Input from '../../FormElements/Input';
+import Button from '../../Button/Button';
 
 const AdvancedSettings = () => {
+	const [isRake, setIsRake] = useState<boolean>(false);
+	const [isAddonAvailable, setIsAddonAvailable] = useState<boolean>(false);
+
 	return (
 		<div className={styles.advancedSettingsContainer}>
-			<div className={styles.advancedSetting}>
-				<p className={styles.question}>Does the organizer take a rake?</p>
-				<input type='checkbox' className={styles.checkbox} />
-			</div>
-			<div className={styles.advancedSetting}>
-				<p className={styles.question}>Are add-ons available?</p>
-				<input type='checkbox' className={styles.checkbox} />
-			</div>
+			{isRake ? (
+				<div className={styles.advancedSettingsInputContainer}>
+					<div className={styles.inputContainer}>
+						<Input label='Percent of rake:' />
+					</div>
+					<Button
+						size='big'
+						className={styles.xButton}
+						onClick={() => setIsRake(false)}
+					>
+						X
+					</Button>
+				</div>
+			) : (
+				<div className={styles.advancedSettingQuestion}>
+					<p className={styles.question}>Does the organizer take a rake?</p>
+					<input
+						type='checkbox'
+						className={styles.checkbox}
+						onChange={() => setIsRake(true)}
+					/>
+				</div>
+			)}
+
+			{isAddonAvailable ? (
+				<div className={styles.advancedSettingsInputContainer}>
+					<div className={styles.inputContainer}>
+						<Input label='Addon value:' />
+					</div>
+					<Button
+						size='big'
+						className={styles.xButton}
+						onClick={() => setIsAddonAvailable(false)}
+					>
+						X
+					</Button>
+				</div>
+			) : (
+				<div className={styles.advancedSettingQuestion}>
+					<p className={styles.question}>Are add-ons available?</p>
+					<input
+						type='checkbox'
+						className={styles.checkbox}
+						onChange={() => setIsAddonAvailable(true)}
+					/>
+				</div>
+			)}
 		</div>
 	);
 };
