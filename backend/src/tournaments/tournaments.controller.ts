1fbfa4fcc516522212c48e4a1f1c29e128fdcfe8
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-08-04 14:14:23 +0200

    feat(backend-tournaments): implement full basic CRUD endpoints

--- backend/src/tournaments/tournaments.controller.ts
+++ backend/src/tournaments/tournaments.controller.ts
@@ -1,13 +1,53 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  BadRequestException,
+  Patch,
+} from '@nestjs/common';
 import { TournamentsService } from './tournaments.service';
 import { CreateTournamentDto } from './dtos/create-tournament.dto';
+import { Types } from 'mongoose';
+import { UpdateTournamentDto } from './dtos/update-tournament-dto';
 
 @Controller('tournaments')
 export class TournamentsController {
   constructor(private tournamentsService: TournamentsService) {}
 
+  private validateID(id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid tournament ID format`);
+    }
+  }
+
   @Post()
   createTournament(@Body() body: CreateTournamentDto) {
     return this.tournamentsService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.tournamentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    this.validateID(id);
+    return this.tournamentsService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    this.validateID(id);
+    return this.tournamentsService.remove(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateTournamentDto) {
+    this.validateID(id);
+    return this.tournamentsService.update(id, body);
   }
 }
--- backend/src/tournaments/tournaments.service.ts
+++ backend/src/tournaments/tournaments.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Tournament, TournamentDocument } from './schemas/tournament.schema';
 import { CreateTournamentDto } from './dtos/create-tournament.dto';
+import { UpdateTournamentDto } from './dtos/update-tournament-dto';
 
 @Injectable()
 export class TournamentsService {
@@ -13,5 +14,46 @@ export class TournamentsService {
 
   async create(tournament: CreateTournamentDto) {
     return this.tournamentModel.create(tournament);
+  }
+
+  async findAll() {
+    return this.tournamentModel.find({});
+  }
+
+  async findOne(id: string) {
+    const tournament = await this.tournamentModel.findById(id);
+
+    if (!tournament) {
+      throw new NotFoundException(`Tournament with ID "${id}" not found`);
+    }
+
+    return tournament;
+  }
+
+  async remove(id: string) {
+    const deletedTournament = await this.tournamentModel.findByIdAndDelete(id);
+
+    if (!deletedTournament) {
+      throw new NotFoundException(`Tournament with ID "${id}" not found`);
+    }
+
+    return deletedTournament;
+  }
+
+  async update(id: string, attrs: UpdateTournamentDto) {
+    const updatedTournament = await this.tournamentModel.findByIdAndUpdate(
+      id,
+      attrs,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    if (!updatedTournament) {
+      throw new NotFoundException(`Tournament with ID "${id}" not found`);
+    }
+
+    return updatedTournament;
   }
 }
