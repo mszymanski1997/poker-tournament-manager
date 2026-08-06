f45e84c795a06de8526778a55dac801135ed9cce
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-08-06 21:00:09 +0200

    feat(frontend-auth): create Auth form UI with login and signup mode toggle

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -1,9 +1,39 @@
+import { Form } from 'react-router-dom';
 import styles from './Auth.module.scss';
+import Input from '../../components/shared/FormElements/Input';
+import Button from '../../components/shared/Button/Button';
+import { useState } from 'react';
 
 const Auth = () => {
+	const [isSignUpMode, setIsSignUpMode] = useState<boolean>(true);
+
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.title}>Sign up</h1>
+			<div className={styles.card}>
+				<h1 className={styles.title}>{isSignUpMode ? 'Sign up' : 'Login'}</h1>
+
+				<Form className={styles.form}>
+					<div className={styles.inputsGroup}>
+						{isSignUpMode && <Input label='Username' type='text' />}
+						<Input label='E-mail' type='email' />
+						<Input label='Password' type='password' />
+						{isSignUpMode && <Input label='Confirm password' type='password' />}
+					</div>
+
+					<div className={styles.actions}>
+						<Button type='submit' size='big'>
+							{isSignUpMode ? 'Sign Up' : 'Login'}
+						</Button>
+						<Button
+							type='button'
+							size='big'
+							onClick={() => setIsSignUpMode(!isSignUpMode)}
+						>
+							{isSignUpMode ? 'Already signed in?' : 'Create account'}
+						</Button>
+					</div>
+				</Form>
+			</div>
 		</div>
 	);
 };
