df0a2fdd796e75956a15a0ccb6f8fc4b6c6b1929
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-08-06 20:12:14 +0200

    fix(frontend-layout): add z-index to nav and fix viewport overflow issue

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -1,7 +1,11 @@
 import styles from './Auth.module.scss';
 
 const Auth = () => {
-	return <h1 className={styles.title}>Auth</h1>;
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Sign up</h1>
+		</div>
+	);
 };
 
 export default Auth;
