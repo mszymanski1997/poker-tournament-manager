6b651a5d7d23ffd269fe8bc559ec0a909500e7e1
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-08-28 21:37:04 +0200

    chore(deps): install @tanstack/react-query and wrap App in QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { PokerProvider } from './store/PokerSettings/PokerProvider';
 import { TimerProvider } from './store/TimerSettings/TimerProvider';
@@ -5,6 +6,8 @@ import Timer from './pages/Timer/Timer';
 import Navigation from './components/shared/Navigation/Navigation';
 import Auth from './pages/Auth/Auth';
 import SavedTournaments from './pages/SavedTournaments/SavedTournaments';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
 	[
@@ -26,11 +29,13 @@ const router = createBrowserRouter(
 
 function App() {
 	return (
-		<TimerProvider>
-			<PokerProvider>
-				<RouterProvider router={router}></RouterProvider>
-			</PokerProvider>
-		</TimerProvider>
+		<QueryClientProvider client={queryClient}>
+			<TimerProvider>
+				<PokerProvider>
+					<RouterProvider router={router}></RouterProvider>
+				</PokerProvider>
+			</TimerProvider>
+		</QueryClientProvider>
 	);
 }
 
